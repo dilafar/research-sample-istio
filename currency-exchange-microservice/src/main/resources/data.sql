2bda7dcf72ab1cf0050294ed275605692632365c
M.R.A.FADHIL <87592439+dilafar@users.noreply.github.com>
2023-05-23 01:31:01 +0530

    first commit

---
+++ currency-exchange-microservice/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple)
+values(10001,'USD','LKR',305);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple)
+values(10002,'EUR','LKR',330);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple)
+values(10003,'AUD','LKR',205);
